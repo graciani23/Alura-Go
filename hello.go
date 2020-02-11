95919bedf2fd69e389ad85eb44ff538a0a3d43e4
graciani23 <gracianigss@gmail.com>
2020-02-11 08:23:59 -0200

    escrevendo arquivos

--- hello.go
+++ hello.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "net/http"
-    "time"
-    "bufio"
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const monitoramentos = 3
@@ -13,104 +15,103 @@ const delay = 5
 
 func main() {
 
-    exibeIntroducao()
-    leSitesDoArquivo()
+	exibeIntroducao()
+	leSitesDoArquivo()
 
-    for { // loop infinito
-        exibeMenu()
-        comando := leComando()
-        
-        switch comando {
-        case 1:
-            iniciarMonitoramento()
-        case 2:
-            fmt.Println("Exibindo Logs...")
-        case 0:
-            fmt.Println("Saindo do programa...")
-            os.Exit(0)
-        default:
-            fmt.Println("Não conheço este comando!")
-            os.Exit(-1)
-       }
+	for { // loop infinito
+		exibeMenu()
+		comando := leComando()
 
-    }
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando!")
+			os.Exit(-1)
+		}
+
+	}
 }
 
 func exibeIntroducao() {
-    nome := "Graciani"
-    versao := 1.1
-    fmt.Println("Olá, sr.", nome)
-    fmt.Println("Este programa está na versão", versao)
+	nome := "Graciani"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
-func exibeMenu()  {
-    fmt.Println("1- Iniciar Monitoramento")
-    fmt.Println("2- Exibir Logs")
-    fmt.Println("0- Sair do Programa")
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 }
 func leComando() int {
-    var comandoLido int
-    fmt.Scan(&comandoLido)
-    fmt.Println("O comando escolhido foi", comandoLido)
-    fmt.Println("")
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
-    return comandoLido
+	return comandoLido
 }
 
 func iniciarMonitoramento() {
-    fmt.Println("Monitorando...")
-    sites := []string{"http://random-status-code.herokuapp.com/", "http://www.alura.com.br/", "http://www.caelum.com.br/"}
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
 
-    for i:=0; i<monitoramentos; i++ {
-        for i, site := range sites {
-            fmt.Println("Testando site", i, ":", site)
-            testaSite(site)
-        }
-        time.Sleep(delay * time.Second)
-        fmt.Println("")
-        
-    }
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
 
-    fmt.Println("")
-    
+	}
+
+	fmt.Println("")
+
 }
 
 func testaSite(site string) {
-    resp, err := http.Get(site) // acessando o site
-    
-    if err != nil {
-        fmt.Println("Ocorreu um erro:", err)
-    }
+	resp, err := http.Get(site) // acessando o site
 
-    if resp.StatusCode == 200 {
-        fmt.Println("Site", site, "foi carregado com sucesso!")
-    } else {
-        fmt.Println("Site:", site, "esta com problemas. Status code:", resp.StatusCode)
-    }
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status code:", resp.StatusCode)
+	}
 }
 
 func leSitesDoArquivo() []string {
-    var sites []string
-    arquivo, err := os.Open("sites.txt")
-    if err != nil {
-        fmt.Println("Ocorreu um erro:", err)
-    }
+	var sites []string
 
-    leitor := bufio.NewReader(arquivo)
+	arquivo, err := os.Open("sites.txt")
 
-    linha, err := leitor.ReadString('\n')
-    if err != nil {
-        fmt.Println("Ocorreu um erro:", err)
-    }
-    fmt.Println(linha)
-    return sites
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+
+	}
+	leitor := bufio.NewReader(arquivo)
+	for {
+		// aspas simples pois as duplas indica string
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+	arquivo.Close()
+	return sites
 }
-
-
-
-
-
-
-
-
-
